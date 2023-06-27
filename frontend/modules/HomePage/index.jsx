0eee759dd2bb22a83aa00c78cf9bc495194a5797
Kirill Ermakovich <106441917+kirillermakovichh@users.noreply.github.com>
2023-06-27 11:35:45 +0300

    Update index.jsx

--- frontend/modules/HomePage/index.jsx
+++ frontend/modules/HomePage/index.jsx
@@ -17,7 +17,7 @@ const HomePage = () => {
   const [playerChoice, setPlayerChoice] = useState(new Object());
 
   //COUNTDOWN
-  const [timeLeft, setEndTime] = useCountdown(new Date().getTime());
+  const [endTime, setEndTime] = useCountdown(new Date().getTime());
   const minutes = Math.floor(timeLeft / 60000) % 60;
   const seconds = Math.floor(timeLeft / 1000) % 60;
 
@@ -113,14 +113,14 @@ const HomePage = () => {
   const onButtonClick = async () => {
     setBetPopupOpen(true);
   };
-
+console.log(endTime);
   const Bet = async (_number, _betType) => {
     try {
       setPlayerChoice((playerChoice.signer = [_number, _betType]));
       setBetPopupOpen(false);
       await bet(_number, _betType);
-      const endTime = new Date().getTime() + 40000; // 40 seconds
-      setEndTime(endTime);
+      const timeLeft = new Date().getTime() + 40000; // 40 seconds
+      setEndTime(timeLeft);
       setCountdown(true);
       await new Promise((resolve) => setTimeout(resolve, 40000));
       Spin();
