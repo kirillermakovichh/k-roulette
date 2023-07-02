a00af83a84d39ac891c9065e04205953c014b29d
Kirill Ermakovich <106441917+kirillermakovichh@users.noreply.github.com>
2023-07-02 12:49:16 +0300

    Update TopBar.jsx

--- frontend/components/TopBar.jsx
+++ frontend/components/TopBar.jsx
@@ -3,7 +3,7 @@ import ConnectButton from "./ConnectButton";
 const TopBar = () => {
   return (
     <div className="fixed top-0 w-full bg-gradient-to-r from-[#028f06]  to-[#055700]">
-      <div className="relative flex w-full items-center px-4 py-2 shadow-xl">
+      <div className="relative flex w-full items-center px-4 py-2 shadow-xl xs:hidden">
         <span className="text-lg text-white drop-shadow-lg font-bold cursor-default">
           KERoulette
         </span>
